b9cfb0dfa947c446afb44f8c3f0922801e559129
Raian Soares <rhayambejhamim@gmail.com>
2021-09-09 22:12:06 -0300

    Curso React

---
+++ src/components/TexInput/index.jsx
@@ -0,0 +1,19 @@
+import P from 'prop-types';
+import './styles.css';
+
+export const TextInput = ({ searchValue, handleChange }) => {
+  return (
+    <input
+      className="text-input"
+      onChange={handleChange}
+      value={searchValue}
+      types="search"
+      placeholder="Consulte aqui seus produtos..."
+    />
+  );
+};
+
+TextInput.propTypes = {
+  searchValue: P.string.isRequired,
+  handleChange: P.func.isRequired,
+};
